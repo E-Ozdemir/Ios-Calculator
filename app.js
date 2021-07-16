87b6b1b2586b1c6e848f217211c13b3a74f827e6
E-Ozdemir <emreclarusway@gmail.com>
2021-07-17 00:03:58 +0200

    some codes added

---
+++ app.js
@@ -0,0 +1,16 @@
+
+function displayFunction(karakter){
+    document.getElementById('display_screen').innerHTML = `${karakter}`;
+
+
+    
+    // const addDisplay = document.querySelectorAll("#display_screen");
+    // addDisplay.push(`${karakter}`);
+}
+
+function resetFunction(){
+    document.getElementById('display_screen').innerHTML = '';
+}
+function equalFunction(){
+
+}
